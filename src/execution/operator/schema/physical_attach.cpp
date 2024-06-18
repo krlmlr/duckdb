c2b051a2194c354f120a9b6fa1f4accbfcf22e3f
Mark <mark.raasveldt@gmail.com>
2024-06-18 09:58:48 +0200

    Merge pull request https://redirect.github.com/duckdb/duckdb/pull/12179 from mariotaddeucci/feature-pyspark-register-function
    [PySpark] Implement UDFRegistration.register method on PySpark api
    
    Upstream-commit: https://github.com/duckdb/duckdb/commit/794fe1efb96e8fe15d1805f197d5df9b06a4f82f

--- src/execution/operator/schema/physical_attach.cpp
+++ src/execution/operator/schema/physical_attach.cpp
@@ -96,6 +96,20 @@ SourceResultType PhysicalAttach::GetData(ExecutionContext &context, DataChunk &c
 		}
 	}
 
+	string extension = "";
+	if (FileSystem::IsRemoteFile(path, extension)) {
+		if (!ExtensionHelper::TryAutoLoadExtension(context.client, extension)) {
+			throw MissingExtensionException("Attaching path '%s' requires extension '%s' to be loaded", path,
+			                                extension);
+		}
+		if (access_mode == AccessMode::AUTOMATIC) {
+			// Attaching of remote files gets bumped to READ_ONLY
+			// This is due to the fact that on most (all?) remote files writes to DB are not available
+			// and having this raised later is not super helpful
+			access_mode = AccessMode::READ_ONLY;
+		}
+	}
+
 	// get the database type and attach the database
 	db_manager.GetDatabaseType(context.client, db_type, *info, config, unrecognized_option);
 	auto attached_db = db_manager.AttachDatabase(context.client, *info, db_type, access_mode);
