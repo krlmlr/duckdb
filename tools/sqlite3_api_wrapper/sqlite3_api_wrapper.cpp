krlmlr/duckdb

commit ac1a71bd4d5de30d7c9786c64ad3a9d152f299dc

SQLite UDF API: several improvements.

tools/sqlite3_api_wrapper/sqlite3_api_wrapper.cpp
@@ -18,6 +18,7 @@
 #include <string>
 #include <chrono>
 #include <cassert>
+#include <climits>
 
 #include "extension_helper.hpp"
 
@@ -50,15 +51,6 @@ struct sqlite3_stmt {
 	vector<string> bound_names;
 	//! The current column values converted to string, used and filled by sqlite3_column_text
 	unique_ptr<sqlite3_string_buffer[]> current_text;
-};
-
-struct sqlite3 {
-	unique_ptr<DuckDB> db;
-	unique_ptr<Connection> con;
-	string last_error;
-	int64_t last_changes = 0;
-	int64_t total_changes = 0;
-	int errCode; /* Most recent error code (SQLITE_*) */
 };
 
 void sqlite3_randomness(int N, void *pBuf) {
@@ -725,7 +717,8 @@ int sqlite3_errcode(sqlite3 *db) {
 	if (!db) {
 		return SQLITE_NOMEM;
 	}
-	return db->last_error.empty() ? SQLITE_OK : SQLITE_ERROR;
+	// return db->last_error.empty() ? SQLITE_OK : SQLITE_ERROR;
+	return db->errCode; //! We should return the exact error code
 }
 
 int sqlite3_extended_errcode(sqlite3 *db) {
@@ -1396,11 +1389,12 @@ SQLITE_API void sqlite3_result_error_code(sqlite3_context *, int) {
 }
 
 SQLITE_API void sqlite3_result_int(sqlite3_context *context, int val) {
-	context->result.u.i = val;
-	context->result.type = SQLiteTypeValue::INTEGER;
+	sqlite3_result_int64(context, val);
 }
 
-SQLITE_API void sqlite3_result_int64(sqlite3_context *, sqlite3_int64) {
+SQLITE_API void sqlite3_result_int64(sqlite3_context *context, sqlite3_int64 val) {
+	context->result.u.i = val;
+	context->result.type = SQLiteTypeValue::INTEGER;
 }
 
 SQLITE_API void sqlite3_result_null(sqlite3_context *context) {
@@ -1457,6 +1451,7 @@ const void *sqlite3_value_blob(sqlite3_value *pVal) {
 
 double sqlite3_value_double(sqlite3_value *pVal) {
 	if (!pVal) {
+		pVal->db->errCode = SQLITE_MISUSE;
 		return 0.0;
 	}
 	switch (pVal->type) {
@@ -1466,29 +1461,45 @@ double sqlite3_value_double(sqlite3_value *pVal) {
 		return (double)pVal->u.i;
 	case SQLiteTypeValue::TEXT:
 	case SQLiteTypeValue::BLOB:
-		return StrictCast::Operation<string_t, double>(pVal->str_t);
+		double res;
+		if (TryCast::Operation<string_t, double>(pVal->str_t, res)) {
+			return res;
+		}
 	default:
+		pVal->db->errCode = SQLITE_MISMATCH;
 		return 0.0;
 	}
 }
 
 int sqlite3_value_int(sqlite3_value *pVal) {
-	return sqlite3_value_int64(pVal);
+	int64_t res = sqlite3_value_int64(pVal);
+	if (res >= std::numeric_limits<int>::min() && res <= std::numeric_limits<int>::max()) {
+		return res;
+	}
+	pVal->db->errCode = SQLITE_MISMATCH;
+	return 0;
 }
 
 sqlite3_int64 sqlite3_value_int64(sqlite3_value *pVal) {
 	if (!pVal) {
+		pVal->db->errCode = SQLITE_MISUSE;
 		return 0;
 	}
+	int64_t res;
 	switch (pVal->type) {
 	case SQLiteTypeValue::INTEGER:
 		return pVal->u.i;
 	case SQLiteTypeValue::FLOAT:
-		return Cast::Operation<double, int64_t>(pVal->u.r);
+		if (TryCast::Operation<double, int64_t>(pVal->u.r, res)) {
+			return res;
+		}
 	case SQLiteTypeValue::TEXT:
 	case SQLiteTypeValue::BLOB:
-		return StrictCast::Operation<string_t, int64_t>(pVal->str_t);
+		if (TryCast::Operation<string_t, int64_t>(pVal->str_t, res)) {
+			return res;
+		}
 	default:
+		pVal->db->errCode = SQLITE_MISMATCH;
 		return 0;
 	}
 }
@@ -1499,6 +1510,7 @@ void *sqlite3_value_pointer(sqlite3_value *, const char *) {
 
 const unsigned char *sqlite3_value_text(sqlite3_value *pVal) {
 	if (!pVal) {
+		pVal->db->errCode = SQLITE_MISUSE;
 		return nullptr;
 	}
 
@@ -1509,9 +1521,8 @@ const unsigned char *sqlite3_value_text(sqlite3_value *pVal) {
 	}
 
 	if (pVal->type == SQLiteTypeValue::INTEGER || pVal->type == SQLiteTypeValue::FLOAT) {
-		Value value((pVal->type == SQLiteTypeValue::INTEGER) ? pVal->u.i : pVal->u.r);
+		Value value = (pVal->type == SQLiteTypeValue::INTEGER) ? Value::BIGINT(pVal->u.i) : Value::DOUBLE(pVal->u.r);
 		if (value.TryCastAs(LogicalType::VARCHAR) == false) {
-			printf("Could not cast 'integer' sqlite_value to 'text'.\n");
 			pVal->db->errCode = SQLITE_NOMEM;
 			return nullptr;
 		}
@@ -1530,7 +1541,7 @@ const unsigned char *sqlite3_value_text(sqlite3_value *pVal) {
 	if (pVal->type == SQLiteTypeValue::NULL_VALUE) {
 		return nullptr;
 	}
-	pVal->db->errCode = SQLITE_ERROR;
+	pVal->db->errCode = SQLITE_MISMATCH;
 	return nullptr;
 }
 

tools/sqlite3_api_wrapper/sqlite3_udf_api/cast_sqlite.cpp
@@ -3,7 +3,7 @@
 #include "duckdb/common/vector_operations/vector_operations.hpp"
 #include "duckdb/common/types/validity_mask.hpp"
 
-bool CastSQLite::IsCastToVarchar(LogicalType type) {
+bool CastSQLite::RequiresCastToVarchar(LogicalType type) {
 	LogicalTypeId type_id = type.id();
 	switch (type_id) {
 	case LogicalTypeId::TINYINT:
@@ -22,27 +22,16 @@ bool CastSQLite::IsCastToVarchar(LogicalType type) {
 	}
 }
 
-void CastSQLite::InputVectorsToVarchar(DataChunk &data_chunk) {
+void CastSQLite::InputVectorsToVarchar(DataChunk &data_chunk, DataChunk &new_chunk) {
+	new_chunk.SetCardinality(data_chunk.size());
+	new_chunk.Initialize(data_chunk.GetTypes());
+
 	for (idx_t i = 0; i < data_chunk.ColumnCount(); ++i) {
-		if (CastSQLite::IsCastToVarchar(data_chunk.data[i].GetType())) {
-			LogicalType vchar_type(LogicalTypeId::VARCHAR);
-			Vector varchar_vector(vchar_type);
-
-			VectorOperations::Cast(data_chunk.data[i], varchar_vector, data_chunk.size(), true);
-
-			bool is_constant_vector = false;
-			if (data_chunk.data[i].GetVectorType() == VectorType::CONSTANT_VECTOR) {
-				is_constant_vector = true;
-				// it's needed to change to FLAT_VECTOR for the COPY works
-				data_chunk.data[i].SetVectorType(VectorType::FLAT_VECTOR);
-			}
-
-			data_chunk.data[i].SetType(vchar_type);
-			VectorOperations::Copy(varchar_vector, data_chunk.data[i], data_chunk.size(), 0, 0);
-
-			if (is_constant_vector) {
-				data_chunk.data[i].SetVectorType(VectorType::CONSTANT_VECTOR);
-			}
+		if (CastSQLite::RequiresCastToVarchar(data_chunk.data[i].GetType())) {
+			new_chunk.data[i].SetType(LogicalTypeId::VARCHAR);
+			VectorOperations::Cast(data_chunk.data[i], new_chunk.data[i], data_chunk.size(), true);
+		} else {
+			new_chunk.data[i].Reference(data_chunk.data[i]);
 		}
 	}
 }

tools/sqlite3_api_wrapper/sqlite3_udf_api/include/cast_sqlite.hpp
@@ -22,8 +22,8 @@ struct CastFromSQLiteValue {
 };
 
 struct CastSQLite {
-	static void InputVectorsToVarchar(DataChunk &data_chunk);
-	static bool IsCastToVarchar(LogicalType type);
+	static void InputVectorsToVarchar(DataChunk &data_chunk, DataChunk &new_chunk);
+	static bool RequiresCastToVarchar(LogicalType type);
 
 	static VectorType ToVectorsSQLiteValue(DataChunk &data_chunk, Vector &result,
 	                                       vector<unique_ptr<vector<sqlite3_value>>> &vec_sqlites,

tools/sqlite3_api_wrapper/sqlite3_udf_api/include/udf_struct_sqlite3.h
@@ -3,11 +3,13 @@
 #include "duckdb/common/types.hpp"
 #include "duckdb/common/types/string_type.hpp"
 #include "duckdb/common/types/string_heap.hpp"
+#include "duckdb.hpp"
+
+#include <memory>
+#include <string>
 
 #include "sqlite3.h"
 #include "sqlite3_value_type.hpp"
-
-using namespace duckdb;
 
 #define SQLITE_UTF8          1 /* IMP: R-37514-35566 */
 #define SQLITE_UTF16LE       2 /* IMP: R-03371-37637 */
@@ -16,6 +18,16 @@ using namespace duckdb;
 #define SQLITE_ANY           5 /* Deprecated */
 #define SQLITE_UTF16_ALIGNED 8 /* sqlite3_create_collation only */
 
+// it was moved to here because the UDF API must know the structure members
+struct sqlite3 {
+	std::unique_ptr<duckdb::DuckDB> db;
+	std::unique_ptr<duckdb::Connection> con;
+	std::string last_error;
+	int64_t last_changes = 0;
+	int64_t total_changes = 0;
+	int errCode; /* Most recent error code (SQLITE_*) */
+};
+
 struct sqlite3_value {
 	union MemValue {
 		double r;  /* Real value used when MEM_Real is set in flags */
@@ -23,13 +35,13 @@ struct sqlite3_value {
 		           // int nZero;          /* Extra zero bytes when MEM_Zero and MEM_Blob set */
 		           // const char *zPType; /* Pointer type when MEM_Term|MEM_Subtype|MEM_Null */
 	} u;
-	SQLiteTypeValue type;
+	duckdb::SQLiteTypeValue type;
 
-	int n;            /* Number of characters in string value, excluding '\0' */
-	string_t str_t;   /* String or BLOB value */
-	char *zMalloc;    /* Space to hold MEM_Str or MEM_Blob if szMalloc>0 */
-	int szMalloc = 0; /* Size of the zMalloc allocation */
-	sqlite3 *db;      /* The associated database connection */
+	int n;                  /* Number of characters in string value, excluding '\0' */
+	duckdb::string_t str_t; /* String or BLOB value */
+	char *zMalloc;          /* Space to hold MEM_Str or MEM_Blob if szMalloc>0 */
+	int szMalloc = 0;       /* Size of the zMalloc allocation */
+	sqlite3 *db;            /* The associated database connection */
 };
 
 struct FuncDef {

tools/sqlite3_api_wrapper/sqlite3_udf_api/sqlite3_udf_wrapper.cpp
@@ -7,15 +7,16 @@
 scalar_function_t SQLiteUDFWrapper::CreateSQLiteScalarFunction(scalar_sqlite_udf_t sqlite_udf, sqlite3 *db_sqlite3,
                                                                void *pApp) {
 	scalar_function_t udf_function = [=](DataChunk &args, ExpressionState &state, Vector &result) -> void {
-		CastSQLite::InputVectorsToVarchar(args);
+		DataChunk cast_chunk;
+		CastSQLite::InputVectorsToVarchar(args, cast_chunk);
 
 		// Orrify all input colunms
-		unique_ptr<VectorData[]> vec_data = args.Orrify();
+		unique_ptr<VectorData[]> vec_data = cast_chunk.Orrify();
 
 		// Vector of sqlite3_value for all input columns
-		vector<unique_ptr<vector<sqlite3_value>>> vec_sqlite(args.ColumnCount());
+		vector<unique_ptr<vector<sqlite3_value>>> vec_sqlite(cast_chunk.ColumnCount());
 		// Casting input data to vectors of sqlite_value
-		VectorType result_vec_type = CastSQLite::ToVectorsSQLiteValue(args, result, vec_sqlite, move(vec_data));
+		VectorType result_vec_type = CastSQLite::ToVectorsSQLiteValue(cast_chunk, result, vec_sqlite, move(vec_data));
 		result.SetVectorType(result_vec_type);
 
 		SQLiteTypeValue res_sqlite_value_type =
@@ -24,22 +25,22 @@ scalar_function_t SQLiteUDFWrapper::CreateSQLiteScalarFunction(scalar_sqlite_udf
 		sqlite3_context context;
 		context.pFunc.pUserData = pApp; // set the function data
 
-		size_t argc = args.ColumnCount();                     // num of args for the UDF
-		vector<sqlite3_value> res_sqlite_values(args.size()); // to store the results from the UDF calls
+		size_t argc = cast_chunk.ColumnCount();                     // num of args for the UDF
+		vector<sqlite3_value> res_sqlite_values(cast_chunk.size()); // to store the results from the UDF calls
 		vector<sqlite3_value> vec_values_to_free; // sqlite_values to free because some memory allocation has happened
+
+		unique_ptr<sqlite3_value *[]> argv = unique_ptr<sqlite3_value *[]>(new sqlite3_value *[argc]);
 
 		// traversing the vector of sqlite values
 		for (idx_t row_idx = 0; row_idx < res_sqlite_values.size(); ++row_idx) {
-
-			sqlite3_value **argv = new sqlite3_value *[argc];
-
 			// create a tuple from sqlite_values
 			for (idx_t col_idx = 0; col_idx < argc; ++col_idx) {
 				argv[col_idx] = &(*(vec_sqlite[col_idx]))[row_idx];
 				argv[col_idx]->db = db_sqlite3;
+				db_sqlite3->errCode = SQLITE_OK;
 			}
 			// call the UDF on that tuple
-			sqlite_udf(&context, argc, argv);
+			sqlite_udf(&context, argc, argv.get());
 
 			// check memory allocatated by the sqlite_values
 			for (idx_t col_idx = 0; col_idx < argc; ++col_idx) {
@@ -68,10 +69,6 @@ scalar_function_t SQLiteUDFWrapper::CreateSQLiteScalarFunction(scalar_sqlite_udf
 			}
 			// getting result
 			res_sqlite_values[row_idx] = context.result;
-
-			if (argc > 0) {
-				delete[] argv;
-			}
 		}
 
 		CastSQLite::ToVectorString(res_sqlite_value_type, res_sqlite_values, result);

commit ae0b4c3074ba6c34bcbce01c8ec364e79ca07a4b

sqlite3_api_wrapper: avoid nullptr dereference (https://redirect.github.com/duckdb/duckdb/pull/14748)
Fixes: ac1a71bd4d("SQLite UDF API: several improvements.")
Upstream-commit: https://github.com/duckdb/duckdb/commit/eebe9bceb34a3ed24f02abd9fdec23408122c0a6

tools/sqlite3_api_wrapper/sqlite3_api_wrapper.cpp
@@ -1745,7 +1745,6 @@ const void *sqlite3_value_blob(sqlite3_value *pVal) {
 
 double sqlite3_value_double(sqlite3_value *pVal) {
 	if (!pVal) {
-		pVal->db->errCode = SQLITE_MISUSE;
 		return 0.0;
 	}
 	switch (pVal->type) {
@@ -1778,7 +1777,6 @@ int sqlite3_value_int(sqlite3_value *pVal) {
 
 sqlite3_int64 sqlite3_value_int64(sqlite3_value *pVal) {
 	if (!pVal) {
-		pVal->db->errCode = SQLITE_MISUSE;
 		return 0;
 	}
 	int64_t res;
@@ -1809,7 +1807,6 @@ void *sqlite3_value_pointer(sqlite3_value *, const char *) {
 
 const unsigned char *sqlite3_value_text(sqlite3_value *pVal) {
 	if (!pVal) {
-		pVal->db->errCode = SQLITE_MISUSE;
 		return nullptr;
 	}
 	if (pVal->type == SQLiteTypeValue::TEXT || pVal->type == SQLiteTypeValue::BLOB) {