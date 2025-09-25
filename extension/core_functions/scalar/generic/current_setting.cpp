krlmlr/duckdb

commit 2cabe4c3422cc8bd2767d4c61f0b4d1e76c054c3

Check autoloaded settings are actually there

extension/core_functions/scalar/generic/current_setting.cpp
@@ -50,9 +50,11 @@ unique_ptr<FunctionData> CurrentSettingBind(ClientContext &context, ScalarFuncti
 	auto key = StringUtil::Lower(StringValue::Get(key_val));
 	Value val;
 	if (!context.TryGetCurrentSetting(key, val)) {
-		Catalog::AutoloadExtensionByConfigName(context, key);
+		auto extension_name = Catalog::AutoloadExtensionByConfigName(context, key);
 		// If autoloader didn't throw, the config is now available
-		context.TryGetCurrentSetting(key, val);
+		if (!context.TryGetCurrentSetting(key, val)) {
+			throw InternalException("Extension %s did not provide the '%s' config setting", extension_name, key);
+		}
 	}
 
 	bound_function.return_type = val.type();

src/execution/operator/helper/physical_reset.cpp
@@ -32,9 +32,13 @@ SourceResultType PhysicalReset::GetData(ExecutionContext &context, DataChunk &ch
 		// check if this is an extra extension variable
 		auto entry = config.extension_parameters.find(name);
 		if (entry == config.extension_parameters.end()) {
-			Catalog::AutoloadExtensionByConfigName(context.client, name);
+			auto extension_name = Catalog::AutoloadExtensionByConfigName(context.client, name);
 			entry = config.extension_parameters.find(name);
-			D_ASSERT(entry != config.extension_parameters.end());
+			if (entry == config.extension_parameters.end()) {
+				// TODO: This is likely to harsh, but given this has a side-effect loaded the extension it should be
+				// somehow brough back to users
+				throw InternalException("Extension %s did not provide the '%s' config setting", extension_name, name);
+			}
 		}
 		ResetExtensionVariable(context, config, entry->second);
 		return SourceResultType::FINISHED;

src/execution/operator/helper/physical_set.cpp
@@ -31,9 +31,13 @@ SourceResultType PhysicalSet::GetData(ExecutionContext &context, DataChunk &chun
 		// check if this is an extra extension variable
 		auto entry = config.extension_parameters.find(name);
 		if (entry == config.extension_parameters.end()) {
-			Catalog::AutoloadExtensionByConfigName(context.client, name);
+			auto extension_name = Catalog::AutoloadExtensionByConfigName(context.client, name);
 			entry = config.extension_parameters.find(name);
-			D_ASSERT(entry != config.extension_parameters.end());
+			if (entry == config.extension_parameters.end()) {
+				// TODO: This is likely to harsh, but given this has a side-effect loaded the extension it should be
+				// somehow brough back to users
+				throw InternalException("Extension %s did not provide the '%s' config setting", extension_name, name);
+			}
 		}
 		SetExtensionVariable(context.client, entry->second, name, scope, value);
 		return SourceResultType::FINISHED;

commit 132032749f7a0cd03627ac296e78cb1cf090ad8e

Avoid throwing on unset extension setting (https://redirect.github.com/duckdb/duckdb/pull/19117)
Fixes a regression introduced in
https://github.com/duckdb/duckdb/commit/2cabe4c3422cc8bd2767d4c61f0b4d1e76c054c3

When extension settings don't have a default value, they are not set,
hence `TryGetCurrentSetting` can return false even if the setting does
exist. This should likely be fixed differently - but having
`TryGetCurrentSetting` return true breaks a bunch of things so that is
for later.

Upstream-commit: https://github.com/duckdb/duckdb/commit/e3d98ae38e16549b4a9f618a53cad67f31ec56b7

extension/core_functions/scalar/generic/current_setting.cpp
@@ -53,10 +53,7 @@ unique_ptr<FunctionData> CurrentSettingBind(ClientContext &context, ScalarFuncti
 	if (!context.TryGetCurrentSetting(key, val)) {
 		auto extension_name = Catalog::AutoloadExtensionByConfigName(context, key);
 		// If autoloader didn't throw, the config is now available
-		if (!context.TryGetCurrentSetting(key, val)) {
-			throw InternalException("Extension %s did not provide the '%s' config setting",
-			                        extension_name.ToStdString(), key);
-		}
+		context.TryGetCurrentSetting(key, val);
 	}
 
 	bound_function.return_type = val.type();