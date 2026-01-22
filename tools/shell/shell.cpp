d413df5e2f2ad0533a84b4a3025d231bf56efa86
Laurens Kuiper <laurens@duckdblabs.com>
2026-01-22 09:35:10 +0100

    chore(shell): Fix comment inconsistencies and typos (https://redirect.github.com/duckdb/duckdb/pull/20574)
    Does what the it says on the tin.
    
    These are a few things I noticed after a first reading of the CLI code.
    
    Thanks for the great work y'all doing!
    
    Upstream-commit: https://github.com/duckdb/duckdb/commit/3f41e38ba71a911f9714476278f41d2941e9dc53

--- tools/shell/shell.cpp
+++ tools/shell/shell.cpp
@@ -1469,7 +1469,7 @@ void ShellState::ResetOutput() {
 				PrintF(PrintOutput::STDERR, "Failed: [%s]\n", zCmd.c_str());
 			} else {
 				/* Give the start/open/xdg-open command some time to get
-				** going before we continue, and potential delete the
+				** going before we continue, and potentially delete the
 				** zTempFile data file out from under it */
 				Sleep(2000);
 			}
@@ -2756,7 +2756,7 @@ bool ShellState::GetBailOnError(InputMode mode) {
 
 /*
 ** Read input from *in and process it.  If *in==0 then input
-** is interactive - the user is typing it it.  Otherwise, input
+** is interactive - the user is typing it in.  Otherwise, input
 ** is coming from a file or device.  A prompt is issued and history
 ** is saved only if input is interactive.  An interrupt signal will
 ** cause this routine to exit immediately, unless input is interactive.
@@ -3114,7 +3114,7 @@ int RunShell(int argc, const char **argv) {
 			if (data.zDbFilename.empty()) {
 				data.zDbFilename = z;
 			} else {
-				/* Excesss arguments are interpreted as SQL (or dot-commands) and
+				/* Excess arguments are interpreted as SQL (or dot-commands) and
 				** mean that nothing is read from stdin */
 				data.readStdin = false;
 				data.stdin_is_interactive = false;
@@ -3171,7 +3171,7 @@ int RunShell(int argc, const char **argv) {
 	data.OpenDB();
 
 	/* Process the initialization file if there is one.  If no -init option
-	** is given on the command line, look for a file named ~/.sqliterc and
+	** is given on the command line, look for a file named ~/.duckdbrc and
 	** try to process it.
 	*/
 	if (data.run_init && !data.ProcessDuckDBRC(data.initFile.empty() ? nullptr : data.initFile.c_str())) {
