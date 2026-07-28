ec527e127c67e104b3d894fe71f766f5a19ae4b5
Kirill Müller <kirill@cynkra.com>
2026-07-28 22:56:28 +0200

    Delete workflows

--- .github/config/extensions/aws.cmake
+++
@@ -1,7 +0,0 @@
-if (NOT MINGW AND NOT ${WASM_ENABLED})
-    duckdb_extension_load(aws
-            LOAD_TESTS
-            GIT_URL https://github.com/duckdb/duckdb-aws
-            GIT_TAG 4e9ae4bdff4476e07cbab81bed795ee524ccf5b0
-            )
-endif()
--- .github/config/extensions/delta.cmake
+++
@@ -1,8 +0,0 @@
-if(NOT MINGW AND NOT ${WASM_ENABLED})
-  duckdb_extension_load(delta
-            GIT_URL https://github.com/duckdb/duckdb-delta
-            GIT_TAG e74cf88ebf64a6638de443025510d8269c31616a
-            SUBMODULES extension-ci-tools
-            APPLY_PATCHES
-  )
-endif()
